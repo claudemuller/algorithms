2b05373237a5db1afc28fc06780deda7f91cbc73
Claude Müller <${GIT_AUTHOR_EMAIL}>
2026-03-05 14:29:04 +0100

    Reduce allocations

--- sorting-algorithms/quicksort/go/pkg/sort/quicksort.go
+++ sorting-algorithms/quicksort/go/pkg/sort/quicksort.go
@@ -2,23 +2,28 @@ package sort
 
 import "slices"
 
+// Quicksort imploys the Quicksort divide and conquer sorting algorithm.
 func Quicksort(data []int) []int {
 	var qs func(arr []int) []int
 
 	qs = func(arr []int) []int {
+		// The base case - if we have 1 or no elements
 		if len(arr) < 2 {
 			return arr
 		}
 
 		halfway := int(float64(len(arr) / 2))
 		pivot := arr[halfway]
-		var less, more []int
+		less := make([]int, 0, halfway)
+		more := make([]int, 0, halfway)
 
 		chop := func(subarr []int) {
 			for _, n := range subarr {
 				if n <= pivot {
+					// Add n to the lower sub-array
 					less = append(less, n)
 				} else {
+					// Add n to the upper sub-array
 					more = append(more, n)
 				}
 			}
